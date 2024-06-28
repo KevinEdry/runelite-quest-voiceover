5f6e027553761e626c18eec8ba84d0d9914a2798
Kevin Orel Edry <techg9@gmail.com>
2024-06-28 06:44:12 -0700

    changed py cli to utilize and create an sqlite db

---
+++ voiceover_cli/database.py
@@ -0,0 +1,38 @@
+import os
+import sqlite3
+from sqlite3 import Connection
+import voiceover_cli.utils as utils
+
+#! This needs to be changed each time we are updating the DB.
+DATABASE_VERSION = "v1"
+
+DATABASE_NAME = "quest_voiceover"
+OUTPUT_DIR = "output_db"
+
+def create_connection() -> Connection:
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+    return sqlite3.connect(f"{OUTPUT_DIR}/{DATABASE_NAME}_{DATABASE_VERSION}.db")
+
+def init_virtual_table(connection: Connection) -> None:
+    cursor = connection.cursor()
+    query = '''
+        CREATE VIRTUAL TABLE IF NOT EXISTS dialogs USING fts4(
+            quest TEXT NOT NULL UNINDEXED,
+            character TEXT NOT NULL,
+            text TEXT NOT NULL,
+            uri TEXT NOT NULL UNINDEXED
+        )
+    '''
+
+    cursor.execute(query)
+    connection.commit()
+
+def insert_quest_voiceover(connection: Connection,quest: str, character: str, text: str, file_name: str) -> None:
+    cursor = connection.cursor()
+    query = f'''
+        INSERT OR REPLACE INTO dialogs(quest, character, text, uri) VALUES ('{utils.escape_single_quotes(quest)}', '{utils.escape_single_quotes(character)}', '{utils.escape_single_quotes(text)}', '{file_name}');
+    '''
+
+    cursor.execute(query)
+    connection.commit()
